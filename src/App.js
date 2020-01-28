d432d6f6ee3b29a636eebb82691c8bd30374edd7
overaction <kmc04@naver.com>
2020-01-28 14:21:49 +0000

    Theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import styled, { createGlobalStyle, css } from "styled-components";
-
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./theme"
 const GlobalStyle = createGlobalStyle`
   body {
     padding:0;
@@ -8,40 +8,40 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const awesomeCard = css`
-    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-    background-color: white;
-    border-radius: 10px;
-    padding: 20px;
-`;
-
 const Container = styled.div`
     height:100vh;
     width:100%;
-    ${awesomeCard};
     background-color:pink;
 `;
 
-const Input = styled.input.attrs(
-    {
-        required: true
-    }
-)`
-    border: none;
-    ${awesomeCard};
+const Card = styled.div`
+  background-color:red;
 `;
+
+const Button = styled.button`
+  border-radius:30px;
+  padding:25px 15px;
+  background-color: ${props => props.theme.successColor};
+`
+
 
 class App extends Component {
   render() {
     return (
     <React.Fragment>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Container>
-        <Input placeholder="hello"></Input>
+        <Form></Form>
       </Container>
+    </ThemeProvider>
     </React.Fragment>
     )
   }
 }
+
+const Form = () => (
+    <Card><Button>Hello</Button></Card>
+)
 
 export default App;
