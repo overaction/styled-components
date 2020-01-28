fec1edbb3e18612676db1cc29400d7bced8fcac1
overaction <kmc04@naver.com>
2020-01-28 13:43:13 +0000

    mixin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled, { createGlobalStyle, keyframes, css } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -8,58 +8,40 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const awesomeCard = css`
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+    background-color: white;
+    border-radius: 10px;
+    padding: 20px;
+`;
+
+const Container = styled.div`
+    height:100vh;
+    width:100%;
+    ${awesomeCard};
+    background-color:pink;
+`;
+
+const Input = styled.input.attrs(
+    {
+        required: true
+    }
+)`
+    border: none;
+    ${awesomeCard};
+`;
+
 class App extends Component {
   render() {
     return (
     <React.Fragment>
       <GlobalStyle />
       <Container>
-        <Button>Hello</Button>
-        <Button dangers rotationTime={1}>Hello!</Button>
-        <Anchor href="http://google.com">Go to google</Anchor>
+        <Input placeholder="hello"></Input>
       </Container>
     </React.Fragment>
     )
   }
 }
-
-const Container = styled.div`
-    height:100vh;
-    width:100%;
-    background-color:pink;
-`;
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus {
-      outline: none;
-  }
-  background-color: ${props => (props.dangers ? "#e74c3c" : "#2ecc71")};
-  ${props2 => {
-    if (props2.dangers) {
-      return css`animation: ${rotation} ${props2.rotationTime}s linear infinite`;
-    }
-  }};
-`;
-
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration:none;
-`;
-
-const rotation = keyframes`
-  from{
-    transform: rotate(0deg);
-  }
-  to{
-    transform: rotate(360deg);
-  }
-`;
 
 export default App;
